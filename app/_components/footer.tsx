cda214bb1243d5252d32885c22baff7bad5f1fc9
lucianobs1 <lucianotetsu@gmail.com>
2024-02-08 00:46:04 -0300

    feat: add footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Footer = () => {
+  return (
+    <footer className="w-full bg-secondary py-6 px-5">
+      <p className="text-xs text-gray-400 font-bold opacity-75">
+        @ 2023 Copyright FSW Barber
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
