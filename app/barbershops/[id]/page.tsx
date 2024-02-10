7bef741e9525c98488eab309318ac4b2f3aad98f
lucianobs1 <lucianotetsu@gmail.com>
2024-02-09 22:04:26 -0300

    feat ensure unathenticated user is redirected to login

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { db } from '@/app/_lib/prisma';
 import BarbershopInfo from './_components/barbershop-info';
 import ServiceItem from './_components/service-item';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 interface BarberShopDetailsPageProps {
   params: {
@@ -11,6 +13,8 @@ interface BarberShopDetailsPageProps {
 const BarberShopDetailsPage = async ({
   params,
 }: BarberShopDetailsPageProps) => {
+  const session = await getServerSession(authOptions);
+
   if (!params.id) {
     // TODO redirecionar para home page
     return null;
@@ -36,7 +40,11 @@ const BarberShopDetailsPage = async ({
 
       <div className="px-5 flex flex-col gap-4 p-6">
         {barbershop.services.map((service) => (
-          <ServiceItem key={service.id} service={service} />
+          <ServiceItem
+            key={service.id}
+            service={service}
+            isAuthenticated={!!session?.user}
+          />
         ))}
       </div>
     </div>
